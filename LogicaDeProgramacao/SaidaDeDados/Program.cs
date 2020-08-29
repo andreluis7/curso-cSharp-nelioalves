323adfb29aafed82678ec833f5cdf9ca10f05e71
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-29 17:31:06 -0300

    Saida de Dados

--- LogicaDeProgramacao/SaidaDeDados/Program.cs
+++ LogicaDeProgramacao/SaidaDeDados/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace SaidaDeDados
 {
@@ -6,7 +7,44 @@ namespace SaidaDeDados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom Dia!");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa Noite!");
+            Console.WriteLine("**************************************");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+
+            // duas casas apos a virgula
+            Console.WriteLine(saldo.ToString("F2"));
+
+            // quatro casas apos a virgula
+            Console.WriteLine(saldo.ToString("F4"));
+
+            //Sair com ponto em vez da virgula
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais.", nome, idade, saldo);
+
+            // mesma frase porem com 2 casas apos a virgula
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+
+            // mesma frase porem com passagem de parametros diferente
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+
+            // mesma frase porem com passagem de parametros diferente
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais.");
+
+            // mesma frase porem com ponto ao inves de virgula
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
         }
     }
 }
