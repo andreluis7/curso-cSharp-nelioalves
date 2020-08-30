934abebf0812331736a2fe9259b9f9e2586341e5
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-30 17:43:21 -0300

    Funcao

---
+++ LogicaDeProgramacao/Funcao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Funcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três numeros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("Maior número é: " + resultado);
+        }
+
+        static int Maior(int n1, int n2, int n3)
+        {
+            int maior;
+
+            if (n1 > n2 && n1 > n3)
+            {
+                maior = n1;
+            } else if (n2 > n3)
+            {
+                maior = n2;
+            }
+            else {
+                maior = n3;
+            }
+
+            return maior;
+        }
+    }
+}
