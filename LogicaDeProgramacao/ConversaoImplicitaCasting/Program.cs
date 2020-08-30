6906b162d871ba5713dc4ed4e81a0315422692f0
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-29 22:00:17 -0300

    Conversao implicita e casting

---
+++ LogicaDeProgramacao/ConversaoImplicitaCasting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConversaoImplicitaCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+
+            a = 5.1;
+
+            b = (float) a;
+
+            Console.WriteLine(b);
+
+            Console.WriteLine("*************************");
+
+            int num1 = 5;
+            int num2 = 2;
+
+            // nesse caso é necessário fazer o cast pois o compilador por ser divisao de 
+            // dois numeros inteiros entende que o resultado e inteiro
+            double resultado = (double) num1 / num2;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
