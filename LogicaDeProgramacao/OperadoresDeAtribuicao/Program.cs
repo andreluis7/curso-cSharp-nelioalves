3b3eb271a60ffe35963bff7db3c9de5b43acb94c
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-29 21:54:34 -0300

    Operadores de Atribuicao

---
+++ LogicaDeProgramacao/OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OperadoresDeAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Console.WriteLine(a);
+
+            a += 2;
+
+            Console.WriteLine(a);
+
+            a *= 3;
+
+            Console.WriteLine(a);
+
+            string s = "ABC";
+
+            Console.WriteLine(s);
+
+            s += "CDE";
+
+            Console.WriteLine(s);
+
+            Console.WriteLine("------------------------------");
+
+            int num = 10;
+
+            Console.WriteLine(num);
+            
+            num++;
+
+            Console.WriteLine(num);
+
+            num--;
+
+            Console.WriteLine(num);
+
+
+        }
+    }
+}
