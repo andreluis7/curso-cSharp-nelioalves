36516c0f2ce712e41de1ee52acd65616d1ed1d53
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-30 22:50:04 -0300

    Problema exemplo

---
+++ Classe-Atributos-Metodos-MembrosEstaticos/ProblemaExemplo/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemaExemplo
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+    }
+}
